063749bcdf1effe7991e7c1f2350ac0efd27a6d1
MinhazH99 <minhazh@hotmail.co.uk>
2023-07-21 17:04:44 +0100

    Update to header to inc navigation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,33 @@
-// CustomHeader.js
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import { format } from "date-fns";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
 
 const Header = () => {
   const navigate = useNavigate();
 
   function handleClick() {
     navigate("/");
+  }
+
+  const goBack = () => {
+    navigate(-1);
+  }
+
+  const goForward = () => {
+    navigate(1);
+  }
+
+  function returnHome() {
+    navigate("/dashboard");
   }
   const [currentTime, setCurrentTime] = useState(new Date());
 
@@ -35,41 +50,34 @@ const Header = () => {
         boxShadow: "0px 3px 5px 2px rgba(0, 0, 0, 0.3)", // Red shadow
       }}
     >
-      <Toolbar
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          color: "#FFFFFF",
-        }}
-      >
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="logo"
-          onClick={handleClick}
-        >
-          <img src="/banklogo.png" alt="Bank Logo" style={{ height: "70px" }} />
-        </IconButton>
-        <Typography
-          variant="h5"
-          sx={{
-            flexGrow: 1,
-            textAlign: "center",
-            marginRight: "140px",
-            color: "#000000",
-          }}
-        >
-          Caledonian Road Branch
-        </Typography>
+      <Toolbar>
+        <Grid container alignItems="center">
+          <Grid item xs={2}>
+            <IconButton edge="start" color="inherit" aria-label="logo" onClick={handleClick}>
+              <img src="/banklogo.png" alt="Bank Logo" style={{ height: "70px" }} />
+            </IconButton>
+          </Grid>
 
-        <Typography
-          variant="h5"
-          sx={{
-            color: "#000000",
-          }}
-        >
-          {format(currentTime, "HH:mm")}
-        </Typography>
+          <Grid item xs={8} container justifyContent="center">
+            <IconButton color="inherit" aria-label="home" onClick={goBack}>
+            <ArrowBackRoundedIcon sx={{height:30,width:30}} />
+            </IconButton>
+              
+            <IconButton color="inherit" aria-label="home" onClick={returnHome}>
+              <HomeRoundedIcon sx={{height:50,width:50}} />
+            </IconButton>
+
+            <IconButton color="inherit" aria-label="home" onClick={goForward}>
+            <ArrowForwardRoundedIcon sx={{height:30,width:30}} />
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={2} container justifyContent="flex-end">
+            <Typography variant="h5" sx={{ color: "#000000" }}>
+              {format(currentTime, "HH:mm")}
+            </Typography>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
