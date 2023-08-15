75f9e40bb4c8e946986ff1972e3ef44253b01674
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-08-15 11:40:13 +0100

    create new payee api

--- frontend/src/pages/CreateNewPayee.js
+++ frontend/src/pages/CreateNewPayee.js
@@ -4,13 +4,70 @@ import Footer from "../components/Footer";
 import TitleBox from "../components/TitleBox";
 import { Grid, Box, TextField, Button, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function CreateNewPayee() {
   const navigate = useNavigate();
 
-  function handleClick() {
-    navigate("/selectamount");
-  }
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [sortCode, setSortCode] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+  const [description, setDescription] = useState("");
+  const [reference, setReference] = useState("");
+
+  const userId = "1"; // Assume this is the logged-in user's ID
+
+  const handleSubmit = async () => {
+    if (
+      !firstName.trim() ||
+      !lastName.trim() ||
+      !sortCode.trim() ||
+      !accountNumber.trim()
+    ) {
+      alert("Please fill in all the required fields.");
+      return;
+    }
+
+    // Format checks
+    if (!/^\d+$/.test(sortCode)) {
+      alert("Sort Code must be a number.");
+      return;
+    }
+
+    if (!/^\d+$/.test(accountNumber)) {
+      alert("Account Number must be a number.");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          second_name: lastName,
+          account_number: accountNumber,
+          user_id: userId,
+          sort_code: sortCode,
+          description: description,
+          reference: reference,
+        }),
+      });
+
+      if (response.status === 201) {
+        alert("Contact created successfully");
+        navigate("/selectamount");
+      } else {
+        alert("Failed to create contact");
+      }
+    } catch (error) {
+      console.error("There was a problem with the server: ", error);
+    }
+  };
+
   const textFieldStyle = {
     backgroundColor: "white",
     // width: "80%",
@@ -67,13 +124,14 @@ function CreateNewPayee() {
             <Grid
               container
               spacing={5}
-              // sx={{ mt: 5, mx: "auto", height: "55vh" }}
               justify="space-between"
               alignItems="center"
             >
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -81,6 +139,8 @@ function CreateNewPayee() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="Sort Code"
+                  value={sortCode}
+                  onChange={(e) => setSortCode(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -88,6 +148,8 @@ function CreateNewPayee() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -95,6 +157,8 @@ function CreateNewPayee() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="Account Number"
+                  value={accountNumber}
+                  onChange={(e) => setAccountNumber(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -102,6 +166,8 @@ function CreateNewPayee() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="Payee Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -109,6 +175,8 @@ function CreateNewPayee() {
               <Grid item xs={12} sm={6}>
                 <TextField
                   label="Payee Reference"
+                  value={reference}
+                  onChange={(e) => setReference(e.target.value)}
                   fullWidth
                   InputProps={{ style: textFieldStyle }}
                 />
@@ -137,7 +205,7 @@ function CreateNewPayee() {
               marginBottom: "auto",
             }}
             style={confirmButtonStyle}
-            onClick={handleClick}
+            onClick={handleSubmit}
           >
             Confirm
           </Button>
